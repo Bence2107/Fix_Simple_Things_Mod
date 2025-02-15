386b0277cc3db6c83611ee23dc709a13d416a7aa
Bence2107 <szabobence003@gmail.com>
2025-02-15 17:10:30 +0100

    Initial Commit

---
+++ src/main/java/net/bence2107/fixsamplethingsmod/FixSampleThingsModClient.java
@@ -0,0 +1,10 @@
+package net.bence2107.fixsamplethingsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FixSampleThingsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
