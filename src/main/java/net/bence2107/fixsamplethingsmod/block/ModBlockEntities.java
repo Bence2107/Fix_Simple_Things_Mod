29dadcacba9de1c4f37cb030a993fbde0d901fd2
Bence2107 <szabobence003@gmail.com>
2025-10-13 22:04:52 +0200

    Rename ModBlockEntities's func

--- src/main/java/net/bence2107/fixsamplethingsmod/block/ModBlockEntities.java
+++ src/main/java/net/bence2107/fixsamplethingsmod/block/ModBlockEntities.java
@@ -12,10 +12,10 @@ import net.minecraft.util.Identifier;
 
 public class ModBlockEntities {
     public static final BlockEntityType<StoneSmelterBlockEntity> STONE_SMELTER_BLOCK_ENTITY =
-            register(StoneSmelterBlockEntity::new, ModBlocks.STONE_SMELTER);
+            registerBlockEntity(StoneSmelterBlockEntity::new, ModBlocks.STONE_SMELTER);
 
-    private static <T extends BlockEntity> BlockEntityType<T> register(FabricBlockEntityTypeBuilder.Factory<? extends T> entityFactory, Block... blocks) {
-        Identifier id = Identifier.of(FixSampleThingsMod.MOD_ID, "stone_oven_block_entity");
+    private static <T extends BlockEntity> BlockEntityType<T> registerBlockEntity(FabricBlockEntityTypeBuilder.Factory<? extends T> entityFactory, Block... blocks) {
+        Identifier id = Identifier.of(FixSampleThingsMod.MOD_ID, "stone_smelter_block_entity");
         return Registry.register(Registries.BLOCK_ENTITY_TYPE, id, FabricBlockEntityTypeBuilder.<T>create(entityFactory, blocks).build());
     }
 
